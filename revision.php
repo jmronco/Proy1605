40dccf4711a58fb8da2ed7922d61ca313d8413f5
jmronco <>
2017-05-16 11:01:51 -0400

    THEY'RE TAKIN' THE HOBBITS TO ISENGARD

---
+++ revision.php
@@ -0,0 +1,23 @@
+<?php
+$PATH='lib';
+
+include $PATH.'lib/Conexion.php';
+include $PATH.'lib/Usuario.php';
+
+$oUsr=new Usuario();
+$oUsr->nombre='nuevo';
+
+if($oUsr->VerificaUsuario())
+    echo "Existe";
+else 
+     echo "No existe";   
+
+/*sesion_start();
+var_dump($_SESSION);*/
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
